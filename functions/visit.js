fcfe07e5124c1c14ff54443b6c4ce58cb52077dc
aidawangcom <18032721531@163.com>
2026-06-19 22:23:04 +0800

    Delete functions directory

--- functions/visit.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest({ request, env }) {
-  let count = await env.COUNTER.get("visits");
-  count = count ? parseInt(count) + 1 : 1;
-  await env.COUNTER.put("visits", count.toString());
-  return new Response(JSON.stringify({ visits: count }), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
